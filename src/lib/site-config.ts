4c46bd626be00871f213dd5f3601ab57166ba848
Ravish Kumar <iamrravish61@gmail.com>
2024-03-04 23:45:40 +0530

    update site-config

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -1,4 +1,124 @@
-export const siteConfig={
-    siteName: "NocodeIITM",
-    siteDescription: ""
+export const siteConfig = {
+    title: "Nocode Club",
+    tagLine: "Build. Fail. Iterate",
+    subTagLine: "We’re here to Increase your Productivity",
+    description:[
+
+    ],
+
+    aboutUs:{
+        titlle: "About Us",
+        subTitle:"From preschool to pre-tertiary, our students enjoy fun, interactive and relevant lessons and are empowered to think beyond the confines of the classroom.",
+        content: [
+            {
+                title: "Vision:",
+                subTitle:"Reduce the failure timeproduct/MVP",
+                description: "The No Code Club envisions a welcoming space where students can easily bring their product ideas to life. Aim to make product development accessible and efficient by utilizing no code tools. And goal is to minimize the time and effort required, allowing students to explore their creativity and innovate with ease."
+            },
+            {
+                title: "Mission:",
+                subTitle:"Create the product building environment where ideas can grow faster",
+                description: "The mission of the No Code Club is to empower students by providing them with the knowledge and resources they need to effectively use no code tools. No-code club committed to offering educational support, guidance, and mentorship throughout the product development journey. By fostering a collaborative community, we strive to create an environment where students can learn, experiment, and succeed in building their own unique products without or minimum the need for coding skills."
+            },
+            {
+                title: "Initiatives:",
+                subTitle:"Innovation, Collaboration, and Creativity",
+                description: "Initiatives like nocode.camp, nocode.labs, nocode.handbook, and nocode.foundry are designed to make the journey of learning and using no code tools easier for students."
+            },
+        ],
+        teams:{
+            heads:[
+                {
+                    name: "Shashank Gupta",
+                    role: "Head & Co-Founder",
+                    description: "",
+                    image: ""
+                },
+                {
+                    name: "Shashank Gupta",
+                    role: "Head & Co-Founder",
+                    description: "",
+                    image: ""
+                },
+            ],
+            leads:[
+                {
+                    name: "Ravish Kumar",
+                    role: "Tech Lead",
+                    description: "",
+                    image: ""
+                },
+                {
+                    name: "Shishir Chaurasiya",
+                    role: "Tech Lead",
+                    description: "",
+                    image: ""
+                },
+                {
+                    name: "Ullas",
+                    role: "Product Lead",
+                    description: "",
+                    image: ""
+                },
+                {
+                    name: "Mayank",
+                    role: "Product Lead",
+                    description: "",
+                    image: ""
+                },
+                {
+                    name: "Anupama",
+                    role: "Groeth Lead",
+                    description: "",
+                    image: ""
+                },
+                {
+                    name: "Jishnu",
+                    role: "Growth Lead",
+                    description: "",
+                    image: ""
+                },
+                
+            ],
+            Associates:[
+                {
+                    name: "Ravish Kumar",
+                    role: "Tech Lead",
+                    description: "",
+                    image: ""
+                },
+                {
+                    name: "Shishir Chaurasiya",
+                    role: "Tech Lead",
+                    description: "",
+                    image: ""
+                },
+                {
+                    name: "Ullas",
+                    role: "Product Lead",
+                    description: "",
+                    image: ""
+                },
+                {
+                    name: "Mayank",
+                    role: "Product Lead",
+                    description: "",
+                    image: ""
+                },
+                {
+                    name: "Anupama",
+                    role: "Groeth Lead",
+                    description: "",
+                    image: ""
+                },
+                {
+                    name: "Jishnu",
+                    role: "Growth Lead",
+                    description: "",
+                    image: ""
+                },
+            ],
+            
+        }
+    }
 }
